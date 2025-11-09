293d63b29f634cf0d68f9f69ad7c522b092739e2
Liladhar300 <liladhar300@gmail.com>
2025-11-09 16:58:42 +0000

    variables in javascript

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const userName = "Liladhar"; 
+// userName = "Patil"; //const variables cannot be reassigned
+
+// const userName = "Leeladhar"; // const cannot be redeclared
+
+let emailId = "liladhar30@gmail.com";
+
+updateEmailId = "liladhar300@gmail.com" // let variable can be re-declared.
+
+// let emailId = "lila"; // let variable cannot be re-assigned.
+
+// console.log("username:", updateEmailId);
+
+console.table([userName, updateEmailId, userId]); // userId cannot be access before initialized. 
+let userId;
